9a547f6f2ed4052d2d832a794fa9dac157837432
Dev <dev@MacBook-Air.local>
2024-03-13 11:39:55 -0400

    feat: product-media-gallery component
    chore: update config files
    
    chore: update variables
    
    chore: update styles
    
    docs: update liquid docs
    
    style: remove whites
    
    chore: update styles

---
+++ components/section-main-product/assets/section-main-product.js
@@ -0,0 +1,5 @@
+// This is the javascript entrypoint for the main-product section.
+// This file and all its inclusions will be processed through postcss
+
+import '@archetype-themes/scripts/config';
+import '@archetype-themes/scripts/modules/product';
