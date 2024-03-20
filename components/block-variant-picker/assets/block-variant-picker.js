0d161dd7ae1965dd2a7351092c09e719c502885c
Dev <dev@MacBook-Air.local>
2024-03-20 13:55:34 -0400

    chore: update block-variant-picker

--- components/block-variant-picker/assets/block-variant-picker.js
+++ components/block-variant-picker/assets/block-variant-picker.js
@@ -1,6 +1,38 @@
 class BlockVariantPicker extends HTMLElement {
   constructor() {
     super();
+    this.addEventListener('change', this.onVariantChange);
+  }
+
+  onVariantChange(event) {
+    this.updateOptions();
+    this.updateMasterId();
+  }
+
+  updateOptions() {
+    this.options = Array.from(this.querySelectorAll('select, fieldset'), (element) => {
+      if (element.tagName === 'SELECT') {
+        return element.value;
+      }
+      if (element.tagName === 'FIELDSET') {
+        return Array.from(element.querySelectorAll('input')).find((radio) => radio.checked)?.value;
+      }
+    });
+  }
+
+  updateMasterId() {
+    this.currentVariant = this.getVariantData().find((variant) => {
+      return !variant.options
+        .map((option, index) => {
+          return this.options[index] === option;
+        })
+        .includes(false);
+    });
+  }
+
+  getVariantData() {
+    this.variantData = this.variantData || JSON.parse(this.querySelector('[type="application/json"]').textContent);
+    return this.variantData;
   }
 }
 
