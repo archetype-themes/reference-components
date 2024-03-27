5a67bf262dd9b565c85937a10f3e92b5e9cdc817
Chris Berthe <chrisberthe@gmail.com>
2024-03-27 14:56:35 -0400

    Add header section

---
+++ components/icon/assets/icon.js
@@ -0,0 +1,25 @@
+class xIcon extends HTMLElement {
+  constructor() {
+    super();
+
+    this.src = this.getAttribute("src");
+    this.name = this.getAttribute("data-name");
+  }
+
+  async connectedCallback() {
+    if (!this.src) return;
+
+    try {
+      console.log(this.src);
+      const response = await fetch(this.src);
+      const svg = await response.text();
+
+      this.innerHTML = svg;
+      this.querySelector("svg").classList.add("icon", `icon--${this.name}`);
+    } catch (error) {
+      console.log(`Error: ${error}`);
+    }
+  }
+}
+
+customElements.define("x-icon", xIcon);
