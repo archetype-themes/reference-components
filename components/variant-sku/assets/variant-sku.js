c18c776ba7b27b50d848584dc2da79e6db63db57
Miguel <miguel.montalvo@archetypethemes.co>
2024-03-27 11:47:00 -0400

    refactor: variant-sku

---
+++ components/variant-sku/assets/variant-sku.js
@@ -0,0 +1,24 @@
+import { subscribe } from '@archetype-themes/scripts/utils/pubsub';
+import { PUB_SUB_EVENTS } from 'components/block-variant-picker';
+
+class VariantSku extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.variantChangeUnsubscriber = subscribe(PUB_SUB_EVENTS.variantChange, (event) => {
+      const { html, sectionId } = event.data;
+      const skuSource = html.querySelector(`[data-section-id="${sectionId}"] variant-sku`);
+      if (skuSource) {
+        this.textContent = skuSource.textContent;
+      }
+    });
+  }
+
+  disconnectedCallback() {
+    this.variantChangeUnsubscriber?.();
+  }
+}
+
+customElements.define('variant-sku', VariantSku);
