42a5fce92c0158d3e2fbbfac9f7c590b1e08b139
Miguel <miguel.montalvo@archetypethemes.co>
2024-04-03 13:22:03 -0400

    test(block-buy-buttons): add tests

---
+++ components/block-buy-buttons/test/block-buy-buttons.spec.js
@@ -0,0 +1,10 @@
+import { test } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://archetype-components.myshopify.com/password');
+  await page.getByLabel('Enter store password').click();
+  await page.getByLabel('Enter store password').fill('archetype');
+  await page.getByLabel('Enter store password').press('Enter');
+  await page.getByRole('link', { name: 'block-buy-buttons' }).click();
+  await page.getByRole('button', { name: 'Add to cart' }).click();
+});
