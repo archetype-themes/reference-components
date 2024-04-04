82ce2b1d96361880b9751d7731672ce1ddf320b8
Miguel <miguel.montalvo@archetypethemes.co>
2024-04-04 11:28:32 -0400

    test(block-price): add tests

---
+++ components/block-price/setup/test/block-price.spec.js
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://archetype-components.myshopify.com/password');
+  await page.getByLabel('Enter store password').click();
+  await page.getByLabel('Enter store password').fill('archetype');
+  await page.getByLabel('Enter store password').press('Enter');
+  await page.getByRole('link', { name: 'block-price' }).click();
+  await expect(page.locator('block-price')).toContainText('$885.95');
+});
