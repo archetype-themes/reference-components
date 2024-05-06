ddb120b15ea81326b1be9b40d546ac996c2689c7
Miguel <miguel.montalvo@archetypethemes.co>
2024-05-06 12:37:55 -0400

    chore: clean up

--- components/block-title/test/block-title.spec.js
+++ components/block-title/test/block-title.spec.js
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test'
 
 test('block-price', async ({ page }) => {
   // Given
-  await page.goto('/?preview_theme_id=167176831280')
+  await page.goto('/')
   await page.getByRole('link', { name: 'block-title' }).click()
   let data = JSON.parse(await page.getByTestId('product-json').innerText())
   // When
